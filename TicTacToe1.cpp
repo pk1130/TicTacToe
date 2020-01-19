f74541da8128f0e6f830ad16a830bbccb21112c8
pk1130 <pk1130@umd.edu>
2020-01-19 20:14:52 +0300

    Added function to draw TicTacToe board

--- TicTacToe1.cpp
+++ TicTacToe1.cpp
@@ -40,4 +40,29 @@ class TicTacToe{
 			}
 }T;
 
+/*
+	This function is used to draw and align the Tic-Tac-Toe board for gameplay
+*/
+void TicTacToe:: board()
+{
+	system("cls");
+	cout << "\n\n\tTic Tac Toe\n\n";
 
+	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+	cout << endl;
+
+	cout << "     |     |     " << endl;
+	cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+
+	cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+
+	cout << "_____|_____|_____" << endl;
+	cout << "     |     |     " << endl;
+
+	cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+
+	cout << "     |     |     " << endl << endl;
+}
